92dee8f59b23c092159f3fadafde667305b14368
Viktor Rees <viktor.rees@gmx.de>
2018-12-27 08:00:09 +0100

    .

--- WoodSpot/src/main.cpp
+++ WoodSpot/src/main.cpp
@@ -1,4 +1,4 @@
-#include <WiFi.h>
+#include "WiFi.h"
 #include <PubSubClient.h>
 
 // use first channel of 16 channels (started from zero)
@@ -18,13 +18,17 @@ unsigned long fadeMillis;
 int brightness = 0; // how bright the LED is
 int fadeAmount = 5; // how many points to fade the LED by
 
+// const char *ssid = "HausRees-Draytek";
+// const char *password = "6422048768813046";
 
-const char *ssid = "Haus-Rees-Draytek";
-const char *password = "6422048768813046";
+char ssid[] = "HausRees-Draytek";     //  your network SSID (name)
+char password[] = "6422048768813046"; // your network password
+
 const char *mqttServer = "192.168.178.84";
 const int mqttPort = 1883;
 const char *mqttUser = "pi-mqtt";
 const char *mqttPassword = "gs5lzvy8";
+const char *topicBrightNess = "wohnung/wohnen/woodspot/brightness";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -43,7 +47,7 @@ void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255)
 {
   // calculate duty, 8191 from 2 ^ 13 - 1
   uint32_t duty = (8191 / valueMax) * min(value, valueMax);
-   Serial.println(duty);
+  Serial.println(duty);
   // write duty to LEDC
   ledcWrite(channel, duty);
 }
@@ -53,6 +57,10 @@ void autoFade()
   if (millis() - fadeMillis > 30)
   { // Serial.print the example variables every 10 seconds
     // set the brightness on LEDC channel 0
+
+    Serial.print("working5 - brightness=");
+    Serial.println(brightness);
+
     ledcAnalogWrite(LEDC_CHANNEL_0, brightness);
 
     // change the brightness for next time through the loop:
@@ -65,42 +73,71 @@ void autoFade()
     }
 
     fadeMillis = millis();
-    Serial.print("working - brightness=");
-    Serial.println(brightness);
+  }
+}
+
+void setup_wifi()
+{
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char *topic, byte *message, unsigned int length)
+{
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off".
+  // Changes the output state according to the message
+  if (String(topic) == topicBrightNess)
+  {
+    Serial.print("Changing output to ");
+    Serial.println(messageTemp);
   }
 }
 
 void setup()
 {
-  Serial.begin(9600);
-  Serial.println("Start Fading!");
+  Serial.begin(115200);
+  setup_wifi();
+  // wifi_setup_scan();
 
   // Setup timer and attach timer to a led pin
   ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
   ledcAttachPin(LED_PIN, LEDC_CHANNEL_0);
+
+  client.setServer(mqttServer, 1883);
+  client.setCallback(callback);
 }
 
 void loop()
 {
-
-  if (millis() - fadeMillis > 30)
-  { // Serial.print the example variables every 10 seconds
-    // set the brightness on LEDC channel 0
-
-    Serial.print("working4 - brightness=");
-    Serial.println(brightness);
-
-    ledcAnalogWrite(LEDC_CHANNEL_0, brightness);
-
-    // change the brightness for next time through the loop:
-    brightness = brightness + fadeAmount;
-
-    // reverse the direction of the fading at the ends of the fade:
-    if (brightness <= 0 || brightness >= 255)
-    {
-      fadeAmount = -fadeAmount;
-    }
-
-    fadeMillis = millis();
-  }
+  autoFade();
 }
